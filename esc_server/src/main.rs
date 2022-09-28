a971fdcf8a4a37d4da508935413f07683674e828
Piotr Podusowski <podusowski@gmail.com>
2022-09-28 21:36:33 +0200

    draft srv

--- esc_server/src/main.rs
+++ esc_server/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::{io::AsyncReadExt, net::TcpListener};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:1234").await.unwrap();
+
+    loop {
+        let (mut client, _) = listener.accept().await.unwrap();
+
+        // Read a "packet" from stream. Packets are implemented by sending a
+        // size first and following it with rest of the data.
+        let size = client.read_u32().await.unwrap() as usize;
+        let mut buf = [0; 1024];
+        client.read_exact(&mut buf[0..size]).await.unwrap();
+    }
 }
