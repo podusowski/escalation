6419e5a94c8dbb34a3ea62f7d704f6179f509033
Piotr Podusowski <podusowski@gmail.com>
2022-09-29 20:44:01 +0200

    fmt

--- esc_common/src/lib.rs
+++ esc_common/src/lib.rs
@@ -3,12 +3,12 @@ use serde::{Deserialize, Serialize};
 #[derive(Deserialize, Serialize, Debug)]
 pub enum Protocol {
     Ping,
-    Pong
+    Pong,
 }
 
 /// `bson` crate can't serialize `enum` directly as it doesn't appear as
 /// a "document" to it. This wrapper fixes it.
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Message {
-    pub value: Protocol
+    pub value: Protocol,
 }
