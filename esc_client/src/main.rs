c3ae3000a907c7679aaa5e0ff48d9da80c3766dc
Piotr Podusowski <podusowski@gmail.com>
2022-09-29 22:51:35 +0200

    show window

--- esc_client/src/main.rs
+++ esc_client/src/main.rs
@@ -1,3 +1,8 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(Color::rgb(0., 0., 0.)))
+        .run();
 }
