5147bf1d2cd45120b94ae9735cdc3bf1b3866247
Piotr Podusowski <podusowski@gmail.com>
2022-09-29 22:19:26 +0200

    spawning in function

--- esc_server/tests/basics.rs
+++ esc_server/tests/basics.rs
@@ -1,18 +1,23 @@
-#[tokio::test]
-async fn basic_test() {
+async fn spawn_server() -> (tokio::process::Child, tokio::net::TcpStream) {
     let path = env!("CARGO_BIN_EXE_esc_server");
-    let _server = tokio::process::Command::new(path)
+    let process = tokio::process::Command::new(path)
         .kill_on_drop(true)
         .spawn()
         .unwrap();
 
-    let mut stream = loop {
+    let stream = loop {
         if let Ok(stream) = tokio::net::TcpStream::connect("localhost:1234").await {
             break stream;
         }
     };
 
-    // Send a Ping.
+    (process, stream)
+}
+
+#[tokio::test]
+async fn basic_test() {
+    let (_, mut stream) = spawn_server().await;
+
     esc_common::send(&mut stream, esc_common::Protocol::Ping).await;
 
     let pong = esc_common::receive(&mut stream).await;
