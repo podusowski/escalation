37ab7134f3529406d1c4d6e5fc813dca5745305b
Piotr Podusowski <podusowski@gmail.com>
2022-09-29 11:32:52 +0200

    connecting to the server

--- esc_server/tests/basics.rs
+++ esc_server/tests/basics.rs
@@ -1,3 +1,5 @@
+use tokio::io::AsyncWriteExt;
+
 #[tokio::test]
 async fn basic_test() {
     let path = env!("CARGO_BIN_EXE_esc_server");
@@ -5,5 +7,14 @@ async fn basic_test() {
         .kill_on_drop(true)
         .spawn()
         .unwrap();
+
+    let mut stream = loop {
+        if let Ok(stream) = tokio::net::TcpStream::connect("localhost:1234").await {
+            break stream;
+        }
+    };
+
+    stream.write_u32(1).await.unwrap();
+
     eprintln!("{}", path);
 }
