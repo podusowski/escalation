2b7a5d9c714319af1e0ed082adf1894fb33c68b6
Piotr Podusowski <podusowski@gmail.com>
2022-09-29 11:08:06 +0200

    fix warnings

--- esc_server/tests/basics.rs
+++ esc_server/tests/basics.rs
@@ -1,7 +1,7 @@
 #[tokio::test]
 async fn basic_test() {
     let path = env!("CARGO_BIN_EXE_esc_server");
-    let mut server = tokio::process::Command::new(path)
+    let _server = tokio::process::Command::new(path)
         .kill_on_drop(true)
         .spawn()
         .unwrap();
