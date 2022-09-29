f0ebdb8aca779931f18ebd94312385ba0dc974c6
Piotr Podusowski <podusowski@gmail.com>
2022-09-29 11:07:44 +0200

    spawn server in integration tests

--- esc_server/tests/basics.rs
+++ esc_server/tests/basics.rs
@@ -1,5 +1,9 @@
-#[test]
-fn basic_test() {
+#[tokio::test]
+async fn basic_test() {
     let path = env!("CARGO_BIN_EXE_esc_server");
+    let mut server = tokio::process::Command::new(path)
+        .kill_on_drop(true)
+        .spawn()
+        .unwrap();
     eprintln!("{}", path);
 }
