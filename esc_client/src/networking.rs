d85721fe2d735ce255a8f4dc67985f41bb60f355
Piotr Podusowski <podusowski@gmail.com>
2022-10-07 15:32:15 +0200

    pinging server

--- esc_client/src/networking.rs
+++ esc_client/src/networking.rs
@@ -4,9 +4,15 @@ use tokio::{net::TcpSocket, runtime::Runtime};
 pub fn networking(runtime: Res<Runtime>) {
     runtime.spawn(async {
         let socket = TcpSocket::new_v4().unwrap();
-        socket
+        let mut stream = socket
             .connect("127.0.0.1:1234".parse().unwrap())
             .await
             .unwrap();
+
+        info!("Connected to server.");
+
+        esc_common::send(&mut stream, esc_common::Protocol::Ping).await;
+        let _ = esc_common::receive(&mut stream).await.unwrap();
+        info!("Got pong.")
     });
 }
