c55eefb1296da95308e17fb624bc5e6a6083accc
Piotr Podusowski <podusowski@gmail.com>
2022-10-07 14:02:37 +0200

    log connection

--- esc_server/src/main.rs
+++ esc_server/src/main.rs
@@ -6,10 +6,11 @@ async fn main() {
     env_logger::init();
 
     let listener = TcpListener::bind("localhost:1234").await.unwrap();
-    log::info!("Listening on port 1234");
+    log::info!("Listening on port 1234.");
 
     loop {
-        let (mut client, _) = listener.accept().await.unwrap();
+        let (mut client, addr) = listener.accept().await.unwrap();
+        log::info!("Connection from '{}' established.", addr);
         let message = esc_common::receive(&mut client).await;
 
         match message {
